0c565e2ba994aba3eb023ade968a67983e6de318
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-06-09 19:16:25 +0300

    Add insert and delete methods

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -8,8 +8,49 @@ class Node {
 
 class Tree {
   constructor(arr) {
-    this.arr = arr;
     this.root = buildTree(arr);
+  }
+  insert(val, root = this.root) {
+    if (val > root.data) {
+      if (root.right === null) root.right = new Node(val);
+      else this.insert(val, root.right);
+      return;
+    }
+    if (val < root.data) {
+      if (root.left === null) root.left = new Node(val);
+      else this.insert(val, root.left);
+      return;
+    }
+  }
+  delete(val) {
+    this.root = this.deleteRec(val, this.root);
+  }
+  deleteRec(val, root = this.root) {
+    if (root === null) return root;
+
+    if (val > root.data) {
+      root.right = this.deleteRec(val, root.right);
+    } else if (val < root.data) {
+      root.left = this.deleteRec(val, root.left);
+    } else {
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+      root.data = this.minValue(root.right);
+      root.right = this.deleteRec(root.data, root.right);
+    }
+    return root;
+  }
+
+  minValue(root = this.root) {
+    let minVal = root.data;
+    while (root.left !== null) {
+      minVal = root.left.data;
+      root = root.left;
+    }
+    return minVal;
   }
 }
 
@@ -43,4 +84,7 @@ function prettyPrint(node, prefix = '', isLeft = true) {
 }
 
 const sampleTree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
-console.log(prettyPrint(sampleTree.root));
+prettyPrint(sampleTree.root);
+
+sampleTree.delete(1);
+prettyPrint(sampleTree.root);
